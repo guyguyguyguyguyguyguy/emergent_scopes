81a9c2b97684afaa12dd2fbe1ad76dff2f73266e
gui <gui@guis-MacBook-Pro.local>
2021-02-08 11:51:51 -0800

    first commit

---
+++ agent.py
@@ -0,0 +1,14 @@
+class actor(object):
+
+    """Docstring for actor. """
+
+    def __init__(self):
+        """TODO: to be defined.
+
+        :Docstring for actor.: TODO
+
+        """
+        MyClass.__init__(self)
+
+        self._Docstring for actor. = Docstring for actor.
+        
