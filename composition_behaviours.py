8c4a35588b7e9d65b24d02020d3e7258cac21378
guyguyguyguyguyguyguy <guy.h.frankel@gmail.com>
2021-02-19 16:10:12 +0200

    i dont know whats happening

--- composition_behaviours.py
+++ composition_behaviours.py
@@ -1,13 +1,29 @@
-from abc import abstractclassmethod, abstractmethod
+from abc import abstractclassmethod, abstractmethod, abstract_attribute
 import random
+from helper import abstractstatic
 from agent import Agent 
 
 
 @abstractclassmethod
-class Behaviour(object):
+class Behaviour:
+   
+    @abstract_attribute
+    def constraints(self) -> dict[str, Callable[List[Agents], bool]]:
+        pass
+
     
+    @abstractstatic
+    def constraint(agents: List[Agent]) -> bool:
+        pass
+
+
+    @abstractstatic
+    def resolution(agents: List[Agent, List[Agent]]) -> None:
+        pass
+
+
     @abstractmethod
-    def step(self, agent: Agent):
+    def step(self, agent: Agent) -> None:
         pass
 
 
@@ -15,13 +31,37 @@ class RandMov(Behaviour):
 
     """
         RandMove provides agents with behaviour representing random movement.
-        Takes an agents current position and returns a new position depending on agents step size.
+        Takes an agents current position and returns a new position depending on agents step size
     """ 
+
+    constraints = {"no overlap": self.constraint} 
 
     def __init__(self) -> None:
         self.step_size = random.randint(1, 10) 
 
+
+    @staticmethod
+    def constraint(agents: List[Agent]) -> bool:
+        """
+            Return either bool of whether conflicts resolved by resolution 
+            orrr
+            Return list of list of conflicting agent with others, although this leads to duplicates
+        """
+        conflicts = []
+        for a in agents:
+            while other := len([o for o in agents if o is not a and o.x != a.x and o.y != a.y]) > 0:
+               # resolution([a].append(others)) 
+                conflicts.append([a].append(others))
+        return conflicts
     
+
+    @staticmethod
+    def resolution(List[Agent, List[Agent]]) -> None:
+       pass 
+        
+        
+
+    @staticmethod
     def step(self, agent : Agent) -> None:
         """
             Move agent that this instance belong to by a random vector, scaled by step size
@@ -29,3 +69,4 @@ class RandMov(Behaviour):
         
         move_vector = random.sample([-1, 0, 1], 2)
         agent.pos += [x*self.step_size for x in move_vector]
+    
